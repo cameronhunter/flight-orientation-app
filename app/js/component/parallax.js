64c12a374072caddc4c257412536c68b6175bd6b
Cameron Hunter <hello@cameronhunter.co.uk>
2013-06-11 00:19:29 -0700

    Initial commit

---
+++ app/js/component/parallax.js
@@ -0,0 +1,38 @@
+define(function (require) {
+
+  'use strict';
+
+  /**
+   * Module dependencies
+   */
+
+  var defineComponent = require('flight/lib/component');
+  var utils = require('flight/lib/utils');
+
+  /**
+   * Module exports
+   */
+
+  return defineComponent(parallax);
+
+  /**
+   * Module function
+   */
+
+  function parallax() {
+
+    this.update = function(e, data) {
+      var leftToRight = (Math.round(data.tiltLR) * -1);
+      var frontToBack = (Math.round(data.tiltFB) * -1);
+      this.$node.css({
+        "-webkit-transform": "translate3d(" + leftToRight + "px, " + frontToBack + "px, 0)",
+        "transform": "translate3d(" + leftToRight + "px, " + frontToBack + "px, 0)"
+      });
+    };
+
+    this.after('initialize', function () {
+      this.on(document, 'orientation-update', utils.throttle(this.update, 1000));
+    });
+  }
+
+});
